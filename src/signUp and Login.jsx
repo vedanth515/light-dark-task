9674cc27bc4b2d5080371474b2c1d34a87101a82
vedanth515 <vedanthkarnataka@gmail.com>
2025-02-19 11:19:10 +0530

    light and dark theme task updated

--- src/signUp and Login.jsx
+++ src/signUp and Login.jsx
@@ -39,8 +39,7 @@ function SignUpLogin() {
                 <div id='nav-right'>
                     <button onClick={handleSignUp}>SignUp</button>
                     <button onClick={handleLogin}>Login</button>
-                    <p onClick={ChangeTheme}>{Theme ? <GoSun />:<FaRegMoon />
-                    }</p>
+                    <p onClick={ChangeTheme}>{Theme ? <GoSun />:<FaRegMoon />}</p>
                 </div>
             </div>
             {
@@ -63,7 +62,20 @@ function SignUpLogin() {
             </form>
             }
 
-           <h5>Hello I am Vedanth I am From Nizamabad</h5>
+            <div id='content'>
+                <div id='content-1'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, libero! Nulla quod illo ipsum modi, alias assumenda hic saepe consequatur inventore debitis eveniet non illum, cumque libero magnam neque excepturi!
+                Fuga deleniti beatae necessitatibus excepturi? Eligendi aspernatur neque, ex eos eaque autem id commodi alias obcaecati tempore necessitatibus sit quos molestiae ut, facilis et explicabo laboriosam accusamus sint atque impedit?
+                Cum aliquid ab illum porro unde, quasi tempora ducimus quas optio nulla vel magnam veniam sed ipsum, rem in neque enim totam. Accusantium soluta obcaecati nihil doloremque blanditiis temporibus voluptatibus!
+                Cupiditate nemo incidunt iure, sit, eaque voluptas enim autem officiis asperiores accusamus dignissimos ab necessitatibus odio, consequatur explicabo deserunt! Placeat dolorem pariatur enim, veniam aliquid repudiandae dicta accusantium. Nam, iusto!
+                Voluptas, qui. Quia rerum voluptatibus non, inventore id, harum modi mollitia animi error incidunt, placeat ipsa? Mollitia eveniet nihil, voluptatibus, et expedita porro at pariatur soluta cum nostrum, consequatur voluptatum?
+                Minima vitae vero accusantium quibusdam nostrum sequi? Est modi, voluptatibus vel qui debitis quam dicta? Repellat porro, fugit recusandae unde eum odio laborum, doloremque laboriosam maxime voluptatibus alias et voluptatum.
+            </p>
+                </div>
+                <div id='image-2'>
+                <img src="https://m.economictimes.com/thumb/msid-95313710,width-1200,height-900,resizemode-4,imgsize-49604/virat-kohli.jpg" width={500}/>
+                </div>
+            </div>
         </div>
     )
 }
